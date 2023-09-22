779ef2b25c7662962cac7f875c61e4656b0f2924
DanielGumbletonWood <78761122+DanielGumbletonWood@users.noreply.github.com>
2023-09-22 13:57:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sysaidclient',
-    version='0.03',
+    version='0.04',
     author='Daniel Gumbleton-Wood, Archie Fergurson',
     description='Wrapper for SysAid API',
     keywords=[],
